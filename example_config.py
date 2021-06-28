e62c86f5d42be0dad387e9eba172e04e5b2ef7d2
Michael Zhou <michaelchouqj@gmail.com>
2021-06-28 09:30:36 -0400

    Fix a comment error

--- example_config.py
+++ example_config.py
@@ -1,5 +1,7 @@
 # Appointment requirement
 APPOINTMENT_TYPES = {"TRANSFER FROM OUT OF STATE", "REAL ID"} # Can be not set and all supported types will be checked
 LOCATION = "NEWARK" # Can be empty and all locations will be checked and the first location that has spot will be booked
+
+# Slack configuration
 SLACK_BOT_TOKEN = "TOKEN_HERE"
 SLACK_CHANNEL_ID = "CHANNEL ID HERE"
